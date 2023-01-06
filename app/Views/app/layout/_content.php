b090372a304ae2ed19708c30888b72acc5f5fb86
Atahan <121398829+atahannb@users.noreply.github.com>
2023-01-06 23:13:51 +0100

    Show dashboard stats only for non-partner users
    Signed-off-by: Atahan <121398829+atahannb@users.noreply.github.com>

--- app/Views/app/layout/_content.php
+++ app/Views/app/layout/_content.php
@@ -43,32 +43,92 @@
         </div>
       </div>
     </div>
-    <div class="col-xl-9 p-0 ps-xl-3">
-      <div class="card border h-100">
-        <div class="card-header border-0 pt-5">
-          <h3 class="card-title align-items-start flex-column">
-            <span class="card-label fw-bold fs-2 mb-1">Weekly Overview</span>
-            <span class="text-muted fw-semibold fs-6">Deposit and withdrawal amounts for this week</span>
-          </h3>
-        </div>
-        <div class="card-body">
-          <?php
-            $withdrawFetchWeekly  = withdrawFetchWeekly();
-            $depositFetchWeekly   = depositFetchWeekly();
-            foreach (json_decode($withdrawFetchWeekly) as $value) { $withdrawFetchWeeklyCount += $value->dayTotal; }
-            foreach (json_decode($depositFetchWeekly) as $value) { $depositFetchWeeklyCount += $value->dayTotal; } ?>
-            <script>
-              const withdrawFetchWeekly= <?=$withdrawFetchWeekly!=""?$withdrawFetchWeekly:'{[]}'; ?>;
-              const depositFetchWeekly = <?=$depositFetchWeekly!=""?$depositFetchWeekly:'{[]}'; ?>;
-            </script>
-            <? if( $withdrawFetchWeeklyCount>0 or $depositFetchWeeklyCount>0 ): ?>
+    <div class="col-xl-9 p-0 ps-xl-3 d-flex flex-column justify-content-between">
+      <div class="row h-100<? partner !== true ? print(' mb-5') : null ?>">
+        <div class="col-12">
+          <div class="card border h-100">
+            <div class="card-header border-0 pt-5">
+              <h3 class="card-title align-items-start flex-column">
+                <span class="card-label fw-bold fs-2 mb-1">Weekly Overview</span>
+                <span class="text-muted fw-semibold fs-6">Deposit and withdrawal amounts for this week</span>
+              </h3>
+            </div>
+            <div class="card-body">
+              <?php
+                $withdrawFetchWeekly  = withdrawFetchWeekly();
+                $depositFetchWeekly   = depositFetchWeekly();
+                foreach (json_decode($withdrawFetchWeekly) as $value) { $withdrawFetchWeeklyCount += $value->dayTotal; }
+                foreach (json_decode($depositFetchWeekly) as $value) { $depositFetchWeeklyCount += $value->dayTotal; } ?>
+                <script>
+                  const withdrawFetchWeekly= <?=$withdrawFetchWeekly!=""?$withdrawFetchWeekly:'{[]}'; ?>;
+                  const depositFetchWeekly = <?=$depositFetchWeekly!=""?$depositFetchWeekly:'{[]}'; ?>;
+                </script>
+                <? if( $withdrawFetchWeeklyCount>0 or $depositFetchWeeklyCount>0 ): ?>
 
-            <div id="chart-dashboard" class="h-100"></div>
-          <?php else: ?>
-            <h3 class="d-flex flex-center text-center text-gray-700 h-100 m-0">No transactions this week</h3>
-          <?php endif ?>
+                <div id="chart-dashboard" class="h-100"></div>
+              <?php else: ?>
+                <h3 class="d-flex flex-center text-center text-gray-700 h-100 m-0">No transactions this week</h3>
+              <?php endif ?>
+            </div>
+          </div>
         </div>
       </div>
+      <? if(partner !== true): ?>
+      <div class="row">
+        <div class="col-12 col-lg-4 mb-5 mb-lg-0">
+          <div class="card card-bordered h-100">
+            <div class="card-header min-h-50px mh-50px p-0">
+              <h5 class="card-title fw-bold flex-center flex-nowrap text-gray-700 w-100 m-0">Monthly Average</h5>
+            </div>
+            <div class="card-body d-flex">
+              <div class="col-6 text-center">
+                <h5 class="card-title fw-semibold text-gray-700 pb-2">Yatırım</h5>
+                <h3 class="fw-bolder text-gray-700 m-0">₺<?= number_format(depositMonthly() / date('t'), 2) ?></h3>
+              </div>
+              <div class="col-6 text-center">
+                <h5 class="card-title fw-semibold text-gray-700 pb-2">Çekim</h5>
+                <h3 class="fw-bolder text-gray-700 m-0">₺<?= number_format(withdrawMonthly() / date('t'), 2) ?></h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-8 mb-5 mb-lg-0">
+          <div class="card card-bordered">
+            <div class="card-header min-h-50px mh-50px p-0">
+              <h5 class="card-title fw-bold flex-center flex-nowrap text-gray-700 w-100 m-0">Transaction Count</h5>
+            </div>
+            <div class="card-body">
+              <div class="row d-flex">
+                <div class="col-6 col-sm-2 text-center p-0 mb-5 mb-sm-0">
+                  <h5 class="card-title fw-semibold text-gray-700 pb-0 pb-sm-2">Deposit</h5>
+                  <h3 class="fw-bolder text-gray-700 m-0"><?= depositProcessMonthly() ?></h3>
+                </div>
+                <div class="col-6 col-sm-2 text-center p-0 mb-5 mb-sm-0">
+                  <h5 class="card-title fw-semibold text-gray-700 pb-0 pb-sm-2">Withdrawal</h5>
+                  <h3 class="fw-bolder text-gray-700 m-0"><?= withdrawProcessMonthly() ?></h3>
+                </div>
+                <div class="col-6 col-sm-2 text-center p-0 mb-5 mb-sm-0">
+                  <h5 class="card-title fw-semibold text-gray-700 pb-0 pb-sm-2">Match</h5>
+                  <h3 class="fw-bolder text-gray-700 m-0"><?= matchProcessMonthly() ?></h3>
+                </div>
+                <div class="col-6 col-sm-2 text-center p-0 mb-5 mb-sm-0">
+                  <h5 class="card-title fw-semibold text-gray-700 pb-0 pb-sm-2">Bank</h5>
+                  <h3 class="fw-bolder text-gray-700 m-0"><?= bankProcessMonthly() ?></h3>
+                </div>
+                <div class="col-6 col-sm-2 text-center p-0">
+                  <h5 class="card-title fw-semibold text-gray-700 pb-0 pb-sm-2">Cross</h5>
+                  <h3 class="fw-bolder text-gray-700 m-0">0</h3>
+                </div>
+                <div class="col-6 col-sm-2 text-center p-0">
+                  <h5 class="card-title fw-semibold text-gray-700 pb-0 pb-sm-2">POS</h5>
+                  <h3 class="fw-bolder text-gray-700 m-0">0</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <? endif; ?>
     </div>
   </div>
 </div>
