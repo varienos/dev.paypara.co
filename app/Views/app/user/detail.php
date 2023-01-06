0c11c383857b6f6cc3782e32259620aff81cd9a3
Atahan <121398829+atahannb@users.noreply.github.com>
2023-01-06 23:39:11 +0100

    Improved logs layout on user profile
    Signed-off-by: Atahan <121398829+atahannb@users.noreply.github.com>

--- app/Views/app/user/detail.php
+++ app/Views/app/user/detail.php
@@ -81,9 +81,14 @@
                                         <li class="nav-item">
                                             <a class="nav-link text-active-primary pb-4 active" data-bs-toggle="tab" href="#kt_user_view_overview_tab">User Details</a>
                                         </li>
+                                        <? if(root === true): ?>
                                         <li class="nav-item">
-                                            <a class="nav-link text-active-primary pb-4" data-bs-toggle="tab" href="#kt_user_view_overview_events_and_logs_tab">Logs</a>
+                                            <a class="nav-link text-active-primary pb-4" data-bs-toggle="tab" href="#transaction-logs">Transaction Logs</a>
                                         </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link text-active-primary pb-4" data-bs-toggle="tab" href="#session-logs">Session Logs</a>
+                                        </li>
+                                        <? endif; ?>
                                         <li class="nav-item ms-auto" <?=(delete_user!==true?"auth=\"false\"":null) ?>>
                                             <button class="btn btn-sm btn-light-danger w-140px h-40px fs-6" data-set="remove" data-id="<?=$user->hash_id ?>">Delete User</button>
                                         </li>
@@ -215,42 +220,12 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="tab-pane fade" id="kt_user_view_overview_events_and_logs_tab" role="tabpanel">
+                                        <? if(root === true): ?>
+                                        <div class="tab-pane fade" id="transaction-logs" role="tabpanel">
                                             <div class="card border pt-4 mb-6 mb-xl-9">
                                                 <div class="card-header border-0">
                                                     <div class="card-title">
-                                                        <h2>Session Logs</h2>
-                                                    </div>
-                                                    <div class="card-toolbar">
-                                                        <button type="button" class="btn btn-sm btn-flex btn-light-primary" id="kt_modal_sign_out_sesions">
-                                                            <span class="svg-icon svg-icon-3">
-                                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect opacity="0.3" x="4" y="11" width="12" height="2" rx="1" fill="currentColor" /><path d="M5.86875 11.6927L7.62435 10.2297C8.09457 9.83785 8.12683 9.12683 7.69401 8.69401C7.3043 8.3043 6.67836 8.28591 6.26643 8.65206L3.34084 11.2526C2.89332 11.6504 2.89332 12.3496 3.34084 12.7474L6.26643 15.3479C6.67836 15.7141 7.3043 15.6957 7.69401 15.306C8.12683 14.8732 8.09458 14.1621 7.62435 13.7703L5.86875 12.3073C5.67684 12.1474 5.67684 11.8526 5.86875 11.6927Z" fill="currentColor" /><path d="M8 5V6C8 6.55228 8.44772 7 9 7C9.55228 7 10 6.55228 10 6C10 5.44772 10.4477 5 11 5H18C18.5523 5 19 5.44772 19 6V18C19 18.5523 18.5523 19 18 19H11C10.4477 19 10 18.5523 10 18C10 17.4477 9.55228 17 9 17C8.44772 17 8 17.4477 8 18V19C8 20.1046 8.89543 21 10 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3H10C8.89543 3 8 3.89543 8 5Z" fill="currentColor" />
-                                                                </svg>
-                                                            </span>
-                                                            Close all sessions</button>
-                                                    </div>
-                                                </div>
-                                                <div class="card-body pt-0 pb-5">
-                                                    <div class="table-responsive">
-                                                        <table class="table align-middle table-row-dashed gy-5" id="sessionTable">
-                                                            <thead class="border-bottom border-gray-200 fs-7 fw-bold">
-                                                                <tr class="text-start text-muted text-uppercase gs-0">
-                                                                    <th class="min-w-100px">Location</th>
-                                                                    <th>Device</th>
-                                                                    <th>IP Address</th>
-                                                                    <th class="min-w-125px">Last Login</th>
-                                                                    <th class="min-w-70px">Session</th>
-                                                                </tr>
-                                                            </thead>
-                                                            <tbody class="fs-6 fw-semibold text-gray-600"></tbody>
-                                                        </table>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="card border pt-4 mb-6 mb-xl-9" style="opacity: .5; cursor: not-allowed;">
-                                                <div class="card-header border-0">
-                                                    <div class="card-title">
-                                                        <h2>Transaction Logs <small class="text-gray-700">(not active)</small></h2>
+                                                        <h2>Transaction Logs</h2>
                                                     </div>
                                                     <div class="card-toolbar">
                                                         <button type="button" class="btn btn-sm btn-light-primary">
@@ -306,6 +281,40 @@
                                                 </div>
                                             </div>
                                         </div>
+                                        <div class="tab-pane fade" id="session-logs" role="tabpanel">
+                                            <div class="card border pt-4 mb-6 mb-xl-9">
+                                                <div class="card-header border-0">
+                                                    <div class="card-title">
+                                                        <h2>Session Logs</h2>
+                                                    </div>
+                                                    <div class="card-toolbar">
+                                                        <button type="button" class="btn btn-sm btn-flex btn-light-primary" id="kt_modal_sign_out_sesions">
+                                                            <span class="svg-icon svg-icon-3">
+                                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect opacity="0.3" x="4" y="11" width="12" height="2" rx="1" fill="currentColor" /><path d="M5.86875 11.6927L7.62435 10.2297C8.09457 9.83785 8.12683 9.12683 7.69401 8.69401C7.3043 8.3043 6.67836 8.28591 6.26643 8.65206L3.34084 11.2526C2.89332 11.6504 2.89332 12.3496 3.34084 12.7474L6.26643 15.3479C6.67836 15.7141 7.3043 15.6957 7.69401 15.306C8.12683 14.8732 8.09458 14.1621 7.62435 13.7703L5.86875 12.3073C5.67684 12.1474 5.67684 11.8526 5.86875 11.6927Z" fill="currentColor" /><path d="M8 5V6C8 6.55228 8.44772 7 9 7C9.55228 7 10 6.55228 10 6C10 5.44772 10.4477 5 11 5H18C18.5523 5 19 5.44772 19 6V18C19 18.5523 18.5523 19 18 19H11C10.4477 19 10 18.5523 10 18C10 17.4477 9.55228 17 9 17C8.44772 17 8 17.4477 8 18V19C8 20.1046 8.89543 21 10 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3H10C8.89543 3 8 3.89543 8 5Z" fill="currentColor" />
+                                                                </svg>
+                                                            </span>
+                                                            Close all sessions</button>
+                                                    </div>
+                                                </div>
+                                                <div class="card-body pt-0 pb-5">
+                                                    <div class="table-responsive">
+                                                        <table class="table align-middle table-row-dashed gy-5" id="sessionTable">
+                                                            <thead class="border-bottom border-gray-200 fs-7 fw-bold">
+                                                                <tr class="text-start text-muted text-uppercase gs-0">
+                                                                    <th class="min-w-100px">Location</th>
+                                                                    <th>Device</th>
+                                                                    <th>IP Address</th>
+                                                                    <th class="min-w-125px">Last Login</th>
+                                                                    <th class="min-w-70px">Session</th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody class="fs-6 fw-semibold text-gray-600"></tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <? endif; ?>
                                     </div>
                                 </div>
                             </div>
