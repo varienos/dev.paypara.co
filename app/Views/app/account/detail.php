4e323891e8a82ccd9484fa5c9f81ccd364336d09
Atahan <121398829+atahannb@users.noreply.github.com>
2023-01-12 00:29:14 +0100

    Improved responsiveness on account details page
    Signed-off-by: Atahan <121398829+atahannb@users.noreply.github.com>

--- app/Views/app/account/detail.php
+++ app/Views/app/account/detail.php
@@ -117,7 +117,7 @@
                                     <i class="bi bi-person fs-2x me-2"></i>Clients
                                   </div>
                                 </td>
-                                <td class="fw-bold text-end"> <?=$update->totalCustomer == 0 ? "No Transaction" : $update->totalCustomer . " person" ?> </td>
+                                <td class="fw-bold text-end"> <?=$update->totalCustomer == 0 ? "No Transaction" : $update->totalCustomer . " client" ?> </td>
                               </tr>
                               <tr>
                                 <td class="text-muted">
@@ -125,7 +125,7 @@
                                     <i class="bi bi-arrow-repeat fs-2x me-2"></i>Transactions
                                   </div>
                                 </td>
-                                <td class="fw-bold text-end"><?=$update->totalProcess == 0 ? "No Transaction" : $update->totalProcess . " tx" ?></td>
+                                <td class="fw-bold text-end"><?=$update->totalProcess == 0 ? "No Transaction" : $update->totalProcess . " total" ?></td>
                               </tr>
                               <tr>
                                 <td class="text-muted">
@@ -414,19 +414,19 @@
                       <div class="tab-pane fade" id="payment-clients" role="tabpanel">
                         <div class="card border">
                           <div class="card-header border-0 pt-6">
-                            <div class="card-title">
-                              <div class="d-flex align-items-center position-relative my-1">
+                            <div class="card-title w-100 w-md-auto">
+                              <div class="d-flex align-items-center w-100 my-1">
                                 <span class="svg-icon svg-icon-1 position-absolute ms-6">
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="currentColor" /><path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="currentColor" />
                                     </svg>
                                 </span>
-                                <input type="text" data-kt-customer-table-filter="search" class="form-control form-control-solid border border-1 w-200px ps-15" placeholder="Search transaction" />
+                                <input type="text" data-kt-customer-table-filter="search" class="form-control form-control-solid border border-1 w-100 w-md-200px ps-15" placeholder="Search transaction" />
                               </div>
                             </div>
-                            <div class="card-toolbar">
-                              <div class="d-flex justify-content-end" data-kt-customer-table-toolbar="base">
-                                <input class="form-control form-control-solid border border-1 mw-225px me-3" placeholder="Select date range" value="<?=date("Y-m-d") ?>" name="transactionDate" id="transactionDate" />
-                                <button type="button" class="btn btn-light me-3" id="filtre" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+                            <div class="card-toolbar w-100 w-md-auto flex-center flex-md-end gap-3">
+                              <div class="d-flex gap-3">
+                                <input class="form-control form-control-solid border border-1 mw-225px" placeholder="Select date range" value="<?=date("Y-m-d") ?>" name="transactionDate" id="transactionDate" />
+                                <button type="button" class="btn btn-light" id="filtre" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
                                   <span class="svg-icon svg-icon-2">
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z" fill="currentColor" />
                                       </svg>
@@ -481,8 +481,6 @@
                                     </div>
                                   </div>
                                 </div>
-                              </div>
-                              <div class="card-toolbar">
                                 <button type="button" class="btn btn-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
                                   <span class="svg-icon svg-icon-2 m-0">
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -496,20 +494,22 @@
                           </div>
                           <style>div.dataTables_length + div.dataTables_info { font-size: 10px !important;}</style>
                           <div class="card-body pt-0">
-                            <table class="table align-middle table-row-dashed fs-6 gy-4 dataTable no-footer" id="accountTransactions">
-                              <thead datatable-head>
-                                <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
-                                  <th class="min-w-125px table-sort-desc">DATE</th>
-                                  <th class="min-w-20px table-sort-desc">TXID</th>
-                                  <th class="min-w-20px table-sort-desc">USER ID</th>
-                                  <th class="min-w-55px table-sort-desc">FIRM</th>
-                                  <th class="min-w-100px table-sort-desc">NAME SURNAME</th>
-                                  <th class="min-w-85px table-sort-desc">AMOUNT</th>
-                                  <th class="min-w-50px table-sort-desc">STATUS</th>
-                                </tr>
-                              </thead>
-                              <tbody class="fw-semibold text-gray-600"></tbody>
-                            </table>
+                            <div class="table-responsive">
+                              <table class="table align-middle table-row-dashed fs-6 gy-4 dataTable no-footer" id="accountTransactions">
+                                <thead datatable-head>
+                                  <tr class="text-gray-400 fw-bold fs-7 text-uppercase gs-0">
+                                    <th class="min-w-150px text-center table-sort-desc">DATE</th>
+                                    <th class="min-w-20px text-center table-sort-desc">TXID</th>
+                                    <th class="min-w-20px text-center table-sort-desc">USER ID</th>
+                                    <th class="min-w-55px text-center table-sort-desc">FIRM</th>
+                                    <th class="min-w-150px text-center table-sort-desc">NAME SURNAME</th>
+                                    <th class="min-w-85px text-center table-sort-desc">AMOUNT</th>
+                                    <th class="min-w-50px text-center table-sort-desc">STATUS</th>
+                                  </tr>
+                                </thead>
+                                <tbody class="fw-semibold text-gray-600 text-center"></tbody>
+                              </table>
+                            </div>
                           </div>
                         </div>
                       </div>
