eee1a690198918c079e4d0a53a10cc4d01a9ef1c
Atahan <121398829+atahannb@users.noreply.github.com>
2023-01-11 17:17:03 +0100

    [WIP] Removed unused view and sass files
    Signed-off-by: Atahan <121398829+atahannb@users.noreply.github.com>

--- app/Views/app/partials/_drawers.php
+++
@@ -1,4 +0,0 @@
-<!--begin::Drawers-->
-<?php include 'partials/drawers/_activity-drawer.php' ?>
-<?php include 'partials/drawers/_chat-messenger.php' ?>
-<!--end::Drawers-->
--- assets/metronic/template/src/js/components/_init.js
+++ assets/metronic/template/src/js/components/_init.js
@@ -3,35 +3,32 @@
 //
 
 // Init components
-var KTComponents = function () {
-    // Public methods
-    return {
-        init: function () {
-            KTApp.init();
-			KTDrawer.init();
-			KTMenu.init();
-			KTScroll.init();
-			KTSticky.init();
-			KTSwapper.init();
-			KTToggle.init();
-			KTScrolltop.init();
-			KTDialer.init();	
-			KTImageInput.init();
-			KTPasswordMeter.init();	
-        }
-    }
-}();
+var KTComponents = (function () {
+  // Public methods
+  return {
+    init: function () {
+      KTApp.init();
+      KTDrawer.init();
+      KTMenu.init();
+      KTScroll.init();
+      KTSticky.init();
+      KTSwapper.init();
+      KTToggle.init();
+      KTPasswordMeter.init();
+    },
+  };
+})();
 
 // On document ready
 if (document.readyState === "loading") {
-	document.addEventListener("DOMContentLoaded", function() {
-		KTComponents.init();
-	});
- } else {
-	KTComponents.init();
- }
+  document.addEventListener("DOMContentLoaded", function () {
+    KTComponents.init();
+  });
+} else {
+  KTComponents.init();
+}
 
- // Init page loader
-window.addEventListener("load", function() {
-    KTApp.initPageLoader();
+// Init page loader
+window.addEventListener("load", function () {
+  KTApp.initPageLoader();
 });
